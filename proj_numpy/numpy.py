7fc12482bfe773d37ae580f6f8d40fe1e865e1e6
mojtabamansori <13mrlien13@gmail.com>
2024-01-17 18:39:55 +0330

    start course

---
+++ proj_numpy/numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = 5
+b = 2
+sum = a + b
+ave = sum / 2
+print(f'aaded aval = {a}, aaded dovm = {b}, jam = {sum}, mingin = {ave}')
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
